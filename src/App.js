a4844d957f3171ad3900e4b2a5a4d4b26ca6b612
Mike Ferrete <michael.ferrete@gmail.com>
2022-04-27 22:08:35 -0800

    completed deleting a movie

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(movie => id !== movie.id));
   }
 
   const addToFavorites = (movie) => {
@@ -49,7 +50,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
